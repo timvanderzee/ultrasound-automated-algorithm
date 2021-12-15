b03051e3f601b5752eb1d007c72cfd5201e5d721
Tim van der Zee <timvdzee@hotmail.com>
2021-12-15 05:08:50 -0700

    improved method for removing diagonal bias

--- Hough/dohough.m
+++ Hough/dohough.m
@@ -1,4 +1,4 @@
-function[Alphas_sel, w_sel] = dohough(fascicle,parms)
+function[alphas, w] = dohough(fascicle,parms)
 
 % This function finds the muscle fascicle angle (alpha) 
 % given the filtered image (fascicle) and parameters (parms)
@@ -34,6 +34,14 @@ re = parms.Emask_radius;
 % hough transform
 [hmat,theta,rho] = hough(fascicle,'RhoResolution',parms.rhores,'Theta',fasangles);
 
+% rotated hough transform (to avoid diagonal bias)
+rot_angle = 20; % [deg]
+fascicle_rot = imrotate(fascicle, rot_angle,'nearest', 'crop');
+[hmat_rot,~,~] = hough(fascicle_rot,'RhoResolution',parms.rhores,'Theta', 90-(45+rot_angle));
+
+% replace diagonal in original (with bias) with rotated one (without bias)
+hmat(:,theta == 45) = hmat_rot;
+
 % angle of the line itself
 gamma = 90 - theta; % with horizontal
 
@@ -53,10 +61,7 @@ for i = 1:length(P)
 end
 
 alphas = gamma(P(:,2));
-
-% don't believe diagonal
-w(alphas == 45) = 0;
-P((alphas == 45),:) = [];
+% alpha = weightedMedian(alphas,w);
 
 %% Optional figure: see which pixels contribute
 N = size(P,1) ;
@@ -70,46 +75,4 @@ if parms.show
     end
 end
 
-%% Rotate and re-compute to avoid diagonal-bias
-rot_angle = 20; % [deg]
-fascicle_rot = imrotate(fascicle,rot_angle);
-[hmat,theta,rho] = hough(fascicle_rot,'RhoResolution',parms.rhores,'Theta',fasangles);
-
-% angle of the line itself
-gamma = 90 - theta - rot_angle; % with horizontal
-
-% relative radius of the ellipse
-r_ellipse_rel = re(1) ./ sqrt(re(1)^2*cosd(gamma).^2 + re(2)^2*sind(gamma).^2);
-
-% correct for relative radius
-hmat_cor = round(hmat ./ repmat(r_ellipse_rel, size(hmat,1),1));
-
-% determine peaks
-P = houghpeaks(hmat_cor, parms.npeaks,'Threshold',0);
-
-% extract angles corresponding to peaks
-w_rot = nan(1,length(P));
-for i = 1:length(P)
-    w_rot(i) = hmat_cor(P(i,1),P(i,2));
-end
-
-alphas_rot = gamma(P(:,2));
-
-% don't believe diagonal
-w_rot(alphas_rot == 45-rot_angle) = 0;
-
-% also ignore angles smaller than minimal angle
-w_rot(alphas_rot < parms.range(1)) = 0;
-
-% give non-rotated diagonal extra credit
-w_rot(alphas_rot == 45) = w_rot(alphas_rot == 45) * 2;
-
-%% lump rotated and non-rotated toghether
-[wtot,i] = sort([w w_rot],'descend');
-alphas_tot = [alphas alphas_rot];
-Alphas = alphas_tot(i);
-Alphas_sel = Alphas(1:parms.npeaks);
-w_sel = wtot(1:parms.npeaks);
-
-    
 end
